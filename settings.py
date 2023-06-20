70f4cab60e236b256c571afac9e257167d7441a4
FLMR1357 <135683859+FLMR1357@users.noreply.github.com>
2023-06-20 10:34:36 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 # URL = "https://api.openai.com/v1/chat/completions"
